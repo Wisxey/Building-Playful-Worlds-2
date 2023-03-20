c9d10257f6d838bcc725d1284cfe9d77151454f2
Wisxey <amy.schoor@student.hku.nl>
2023-03-20 17:07:22 +0100

    afronding fvx en post processing scene 2 etc

---
+++ Assets/QuitButtonLoaderScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+public class QuitbuttonLoaderScript : MonoBehaviour
+{
+    public void button_exit()
+    {
+        Application.Quit();
+    }
+}
+
